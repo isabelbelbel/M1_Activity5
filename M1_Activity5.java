5ad9341ed807e9c1ac1e7dc74df000b8b39d7dc5
Isabel C. Morales <isabel.morales@robinsonsbank.com.ph>
2025-12-02 16:45:32 +0800

    Initial Commit

---
+++ M1_Activity5.java
@@ -0,0 +1,19 @@
+package com.bpi.hello;
+
+import java.util.*;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= 50; i++){
+			sum += i;
+		}
+		 
+		 System.out.print("Sum = " + sum);
+	}
+
+}
